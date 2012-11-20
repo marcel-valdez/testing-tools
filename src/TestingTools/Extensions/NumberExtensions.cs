8f958e1dcd5046a7a82818b810c36f8f85a4ed5e
Marcel Valdez <marcel.valdez@live.com>
2012-11-19 23:40:06 -0600

    May have fixed a bug.

--- src/TestingTools/Extensions/NumberExtensions.cs
+++ src/TestingTools/Extensions/NumberExtensions.cs
@@ -13,19 +13,34 @@ namespace TestingTools.Extensions
         public static IVerifiable<int> IsGreaterThanOrEqual(this IAssertion<int> number, int compared, string message = "")
         {
             return new Verifiable<int>(number, target =>
-            Verify.That(target >= compared).IsTrue(string.Format("Value wasn't greater than or equal {0}, it was {1}\n{2}", compared, number, message)));   
+            Verify.That(target >= compared)
+                  .IsTrue(string.Format(
+                                    "Value wasn't greater than or equal {0}, it was {1}\n{2}", 
+                                    compared, 
+                                    number, 
+                                    message)));
         }
 
         public static IVerifiable<int> IsLessThanOrEqual(this IAssertion<int> number, int compared, string message = "")
         {
             return new Verifiable<int>(number, target =>
-            Verify.That(target <= compared).IsTrue(string.Format("Value wasn't less than or equal to {0}, it was {1}\n{2}", compared, number, message)));   
+            Verify.That(target <= compared)
+                  .IsTrue(string.Format(
+                                    "Value wasn't less than or equal to {0}, it was {1}\n{2}",
+                                    compared,
+                                    number, 
+                                    message)));
         }
 
         public static IVerifiable<int> IsLessThan(this IAssertion<int> number, int compared, string message = "")
         {
             return new Verifiable<int>(number, target =>
-            Verify.That(target < compared).IsTrue(string.Format("Value wasn't less than {0}, it was {1}\n{2}", compared, number, message)).Now());
+            Verify.That(target < compared)
+                  .IsTrue(string.Format(
+                                    "Value wasn't less than {0}, it was {1}\n{2}",
+                                    compared,
+                                    number, 
+                                    message)));
         }
     }
 }
