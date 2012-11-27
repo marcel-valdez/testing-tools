5a6789c1d279e99376572d3a043124e88eea15aa
Marcel Valdez <marcel.valdez@live.com>
2012-11-26 23:37:43 -0600

    Fixed a bug where the number extensions failed completely :s

---
+++ src/TestingTools.Tests/NumberExtensionsTest.cs
@@ -0,0 +1,135 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="NumberExtensionsTest.cs" company="">
+// Copyright 2012 Marcel Valdez
+// </copyright>
+// -----------------------------------------------------------------------
+namespace TestingTools.Tests
+{
+    using NUnit.Framework;
+    using TestingTools.Extensions;
+    using TestingTools.Core;
+    using System;
+
+    /// <summary>
+    /// Has all the tests for the number extensions.
+    /// </summary>
+    public class NumberExtensionsTest
+    {        
+        // IsLess
+        /// <summary>
+        /// Tests if it can check less than.
+        /// </summary>
+        /// <param name="targetNumber">The target number.</param>
+        /// <param name="comparedNumber">The compared number.</param>
+        /// <param name="fail">if set to <c>true</c> it should [fail].</param>
+        [TestCase(-1, 0, false)]
+        [TestCase(0, 1, false)]
+        // IsEqual
+        [TestCase(0, 0, true)]
+        // IsGreater                
+        [TestCase(1, 0, true)]
+        public void TestIfItCanCheckLessThan(int targetNumber, int comparedNumber, bool fail)
+        {
+            // Arrange            
+            IAssertion<int> target = Verify.That(targetNumber);
+
+            // Act
+            IVerifiable<int> actual = NumberExtensions.IsLessThan(target, comparedNumber);
+
+            // Assert
+            AssertNumberComparison(fail, actual);
+        }
+
+        /// <summary>
+        /// Tests if it can check less than or equal.
+        /// </summary>
+        /// <param name="targetNumber">The target number.</param>
+        /// <param name="comparedNumber">The compared number.</param>
+        /// <param name="fail">if set to <c>true</c> it should [fail].</param>        
+        // IsLess
+        [TestCase(-1, 0, false)]
+        [TestCase(0, 1, false)]
+        // IsEqual
+        [TestCase(0, 0, false)]
+        // IsGreater                
+        [TestCase(1, 0, true)]
+        public void TestIfItCanCheckLessThanOrEqual(int targetNumber, int comparedNumber, bool fail)
+        {
+            // Arrange            
+            IAssertion<int> target = Verify.That(targetNumber);
+
+            // Act
+            IVerifiable<int> actual = NumberExtensions.IsLessThanOrEqual(target, comparedNumber);
+
+            // Assert
+            AssertNumberComparison(fail, actual);
+        }
+        
+        /// <summary>
+        /// Tests if it can check greater than.
+        /// </summary>
+        /// <param name="targetNumber">The target number.</param>
+        /// <param name="comparedNumber">The compared number.</param>
+        /// <param name="fail">if set to <c>true</c> it should [fail].</param>        
+        // IsLess
+        [TestCase(-1, 0, true)]
+        [TestCase(0, 1, true)]
+        // IsEqual
+        [TestCase(0, 0, true)]
+        // IsGreater                
+        [TestCase(1, 0, false)]
+        public void TestIfItCanCheckGreaterThan(int targetNumber, int comparedNumber, bool fail)
+        {
+            // Arrange            
+            IAssertion<int> target = Verify.That(targetNumber);
+
+            // Act
+            IVerifiable<int> actual = NumberExtensions.IsGreaterThan(target, comparedNumber);
+
+            AssertNumberComparison(fail, actual);
+        }
+
+        // IsLess
+        /// <summary>
+        /// Tests if it can check greater than or equal.
+        /// </summary>
+        /// <param name="targetNumber">The target number.</param>
+        /// <param name="comparedNumber">The compared number.</param>
+        /// <param name="fail">if set to <c>true</c> it should [fail].</param>
+        [TestCase(-1, 0, true)]
+        [TestCase(0, 1, true)]
+        // IsEqual
+        [TestCase(0, 0, false)]
+        // IsGreater                
+        [TestCase(1, 0, false)]
+        public void TestIfItCanCheckGreaterThanOrEqual(int targetNumber, int comparedNumber, bool fail)
+        {
+            // Arrange            
+            IAssertion<int> target = Verify.That(targetNumber);
+
+            // Act
+            IVerifiable<int> actual = NumberExtensions.IsGreaterThanOrEqual(target, comparedNumber);
+
+            // Assert
+            AssertNumberComparison(fail, actual);
+        }
+
+        /// <summary>
+        /// Asserts that the number comparison throws (or not) an AssertionException.
+        /// </summary>
+        /// <param name="throws">if set to <c>true</c> if it should throw an AssertionException.</param>
+        /// <param name="verification">The verification to assert for an exception.</param>
+        private static void AssertNumberComparison(bool throws, IVerifiable<int> verification)
+        {
+            // Assert
+            if (throws)
+            {
+                Assert.Throws<AssertionException>(() => verification.Now());
+            }
+            else
+            {
+                Assert.DoesNotThrow(() => verification.Now());
+            }
+        }
+    }
+}
--- src/TestingTools/Extensions/NumberExtensions.cs
+++ src/TestingTools/Extensions/NumberExtensions.cs
@@ -1,46 +1,57 @@
 namespace TestingTools.Extensions
 {
     using TestingTools.Core;
+    using NUnit.Framework;
 
     public static class NumberExtensions
     {
         public static IVerifiable<int> IsGreaterThan(this IAssertion<int> number, int compared, string message = "")
         {
             return new Verifiable<int>(number, target =>
-            Verify.That(target > compared).IsTrue(string.Format("Value wasn't greater than {0}, it was {1}\n{2}", compared, number, message)).Now());
+            Assert.That(number, 
+                        Is.GreaterThan(compared), 
+                        string.Format("Value wasn't greater than {0}, it was {1}\n{2}", 
+                            compared, 
+                            number, 
+                            message)));
         }
 
         public static IVerifiable<int> IsGreaterThanOrEqual(this IAssertion<int> number, int compared, string message = "")
         {
             return new Verifiable<int>(number, target =>
-            Verify.That(target >= compared)
-                  .IsTrue(string.Format(
-                                    "Value wasn't greater than or equal {0}, it was {1}\n{2}", 
-                                    compared, 
-                                    number, 
+                Assert.That(number, 
+                            Is.GreaterThanOrEqualTo(compared), 
+                            string.Format(
+                                    "Value wasn't greater than or equal {0}, it was {1}\n{2}",
+                                    compared,
+                                    number,
                                     message)));
         }
 
         public static IVerifiable<int> IsLessThanOrEqual(this IAssertion<int> number, int compared, string message = "")
         {
             return new Verifiable<int>(number, target =>
-            Verify.That(target <= compared)
-                  .IsTrue(string.Format(
-                                    "Value wasn't less than or equal to {0}, it was {1}\n{2}",
+                Assert.That(number, 
+                            Is.LessThanOrEqualTo(compared), 
+                            string.Format(
+                                    "Value wasn't less than or equal {0}, it was {1}\n{2}",
                                     compared,
-                                    number, 
+                                    number,
                                     message)));
         }
 
         public static IVerifiable<int> IsLessThan(this IAssertion<int> number, int compared, string message = "")
         {
             return new Verifiable<int>(number, target =>
-            Verify.That(target < compared)
-                  .IsTrue(string.Format(
+            {
+                Assert.That(target, 
+                            Is.LessThan(compared), 
+                            string.Format(
                                     "Value wasn't less than {0}, it was {1}\n{2}",
                                     compared,
-                                    number, 
-                                    message)));
+                                    number,
+                                    message));
+            });
         }
     }
 }
