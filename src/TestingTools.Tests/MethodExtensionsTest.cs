e1e125c1a7f5efeb2de8d84cb1c09bb20afa4ee3
Marcel Valdez Orozco <marcel.valdez@live.com>
2013-02-02 23:09:45 -0600

    Removed bad file (OrVerifiable) and added tests for console output.

---
+++ src/TestingTools.Tests/MethodExtensionsTest.cs
@@ -0,0 +1,164 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="MethodExtensionsTest.cs" company="">
+// Marcel Valdez, Copyright (r) 2012
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace TestingTools.Tests
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Text;
+  using NUnit.Framework;
+  using TestingTools.Core;
+  using TestingTools.Extensions;
+
+  /// <summary>
+  /// This class contains the unit tests for the MethodExtensions class
+  /// </summary>
+  [TestFixture]
+  public class MethodExtensionsTest
+  {
+
+    /// <summary>
+    /// Tests if it can verify console output.
+    /// </summary>
+    /// <param name="expectedWritten">The expected written.</param>
+    /// <param name="actualWritten">The actual written.</param>
+    /// <param name="shouldPass">if set to <c>true</c> [should pass].</param>    
+    [Test, Category("IntegrationTest")]
+    [TestCase("bad", "test", false)]
+    [TestCase("test", "test", true)]
+    public void TestIfItCanVerifyExactConsoleOutput(string expectedWritten, string actualWritten, bool shouldPass)
+    {
+      // Arrange
+      Action targetAct = () => Console.Write(actualWritten);
+      var target = Verify.That(targetAct).WritesExactlyToConsole(expectedWritten);
+
+      // Act
+      TestDelegate act = () => target.Now();
+
+      // Assert
+      if (shouldPass)
+      {
+        Assert.DoesNotThrow(act);
+      }
+      else
+      {
+        AssertionException execption = Assert.Throws<AssertionException>(act);
+      }
+    }
+
+    /// <summary>
+    /// Tests if it can verify console output.
+    /// </summary>
+    /// <param name="expectedWritten">The expected written.</param>
+    /// <param name="actualWritten">The actual written.</param>
+    /// <param name="shouldPass">if set to <c>true</c> [should pass].</param>    
+    [Test, Category("IntegrationTest")]
+    [TestCase("bad", "test", false)]
+    [TestCase("bad", "", false)]
+    [TestCase("test", "test", true)]
+    [TestCase("test", "testA", true)]
+    [TestCase("test", "Atest", true)]
+    [TestCase("test", "AtestA", true)]
+    public void TestIfItCanVerifyConsoleOutput(string expectedWritten, string actualWritten, bool shouldPass)
+    {
+      // Arrange
+      Action targetAct = () => Console.Write(actualWritten);
+      var target = Verify.That(targetAct).WritesToConsole(expectedWritten);
+
+      // Act
+      TestDelegate act = () => target.Now();
+
+      // Assert
+      if (shouldPass)
+      {
+        Assert.DoesNotThrow(act);
+      }
+      else
+      {
+        AssertionException execption = Assert.Throws<AssertionException>(act);
+      }
+    }
+
+    /// <summary>
+    /// Tests if it can verify console output.
+    /// </summary>
+    /// <param name="expectedWritten">The expected written.</param>
+    /// <param name="actualWritten">The actual written.</param>
+    /// <param name="shouldPass">if set to <c>true</c> [should pass].</param>    
+    [Test, Category("IntegrationTest")]
+    [TestCase("bad", "test", true)]
+    [TestCase("test", "test", false)]
+    public void TestIfItCanVerifyExactBadConsoleOutput(string expectedWritten, string actualWritten, bool shouldPass)
+    {
+      // Arrange
+      Action targetAct = () => Console.Write(actualWritten);
+      var target = Verify.That(targetAct).DoesntWriteExactlyToConsole(expectedWritten);
+
+      // Act
+      TestDelegate act = () => target.Now();
+
+      // Assert
+      if (shouldPass)
+      {
+        Assert.DoesNotThrow(act);
+      }
+      else
+      {
+        AssertionException execption = Assert.Throws<AssertionException>(act);
+      }
+    }
+
+    /// <summary>
+    /// Tests if it can verify console output.
+    /// </summary>
+    /// <param name="expectedWritten">The expected written.</param>
+    /// <param name="actualWritten">The actual written.</param>
+    /// <param name="shouldPass">if set to <c>true</c> [should pass].</param>    
+    [Test, Category("IntegrationTest")]
+    [TestCase("bad", "test", true)]
+    [TestCase("bad", "", true)]
+    [TestCase("test", "test", false)]
+    [TestCase("test", "testA", false)]
+    [TestCase("test", "Atest", false)]
+    [TestCase("test", "AtestA", false)]
+    public void TestIfItCanVerifyBadConsoleOutput(string expectedWritten, string actualWritten, bool shouldPass)
+    {
+      // Arrange
+      Action targetAct = () => Console.Write(actualWritten);
+      var target = Verify.That(targetAct).DoesntWriteToConsole(expectedWritten);
+
+      // Act
+      TestDelegate act = () => target.Now();
+
+      // Assert
+      if (shouldPass)
+      {
+        Assert.DoesNotThrow(act);
+      }
+      else
+      {
+        AssertionException execption = Assert.Throws<AssertionException>(act);
+      }
+    }
+
+    [Test]
+    public void TestIfItSendsExceptionMessage()
+    {
+      // Arrange
+      string expectedMsg = "test message";
+      Action targetAct = () => Console.Write("0");
+      var target = Verify.That(targetAct).WritesExactlyToConsole("1", expectedMsg);
+
+      // Act
+      TestDelegate act = (TestDelegate)(() => target.Now());
+      AssertionException exception = Assert.Throws<AssertionException>(act);
+
+      // Assert      
+      Assert.That(exception.Message, Is.StringContaining(expectedMsg));
+    }
+  }
+}
--- src/TestingTools/Extensions/MethodExtensions.cs
+++ src/TestingTools/Extensions/MethodExtensions.cs
@@ -1,105 +1,228 @@
 namespace TestingTools.Extensions
 {
-    using System;
-    using NUnit.Framework;
-    using TestingTools.Core;
+  using System;
+  using NUnit.Framework;
+  using TestingTools.Core;
+  using System.IO;
 
-    public static class Method
+  public static class MethodExtensions
+  {
+    /// <summary>
+    /// Tests tha a Func<typeparamref name="T"></typeparamref> throws an exception.
+    /// </summary>
+    /// <typeparam name="T">Return type</typeparam>
+    /// <param name="function">The function.</param>
+    /// <param name="message">The message.</param>
+    public static IVerifiable<Func<T>> ThrowsException<T>(
+      this IAssertion<Func<T>> function, 
+      string message = "")
     {
-        /// <summary>
-        /// Tests tha a Func<typeparamref name="T"></typeparamref> throws an exception.
-        /// </summary>
-        /// <typeparam name="T">Return type</typeparam>
-        /// <param name="function">The function.</param>
-        /// <param name="message">The message.</param>
-        public static IVerifiable<Func<T>> ThrowsException<T>(this IAssertion<Func<T>> function, string message = "")
-        {
-            return new Verifiable<Func<T>>(function, target =>
-                    Assert.IsNotNull(
-                        target.GetThrownException(),
-                        "La función debió lanzar una excepción, pero no fue así.\n" + message));
-        }
-
-        /// <summary>
-        /// Tests that an Action should throw an exception
-        /// </summary>
-        /// <param name="action">The action.</param>
-        /// <param name="message">The message.</param>
-        public static IVerifiable<Action> ThrowsException(this IAssertion<Action> action, string message = "")
-        {
-            return new Verifiable<Action>(action, target =>
-            {
-                Assert.IsNotNull(target.GetThrownException(), message);
-            });
-        }
-
-        /// <summary>
-        /// Tests tha a Func<typeparamref name="T"/> does not throw an exception.
-        /// </summary>
-        /// <typeparam name="T">Return type</typeparam>
-        /// <param name="function">The function.</param>
-        /// <param name="message">The message.</param>
-        public static IVerifiable<Func<T>> DoesNotThrowException<T>(this IAssertion<Func<T>> function, string message = "")
-        {
-            return new Verifiable<Func<T>>(function, target =>
-                    Assert.IsNull(
-                        target.GetThrownException(),
-                        "La función no debió lanzar una excepción, pero sí lo hizo.\n" + message));
-        }
-
-        /// <summary>
-        /// Tests that an Action should not throw an exception
-        /// </summary>
-        /// <param name="action">The action.</param>
-        /// <param name="message">The message.</param>
-        public static IVerifiable<Action> DoesNotThrowException(this IAssertion<Action> action, string message = "")
-        {
-            return new Verifiable<Action>(action, target =>
-            {
-                Assert.IsNull(target.GetThrownException(), message);
-            });
-        }
-
-        /// <summary>
-        /// Gets the exception thrown.
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="function">The function.</param>
-        /// <returns></returns>
-        private static Exception GetThrownException<T>(this Func<T> function)
-        {
-            Exception x = null;
-            try
-            {
-                function();
-            }
-            catch (Exception ex)
-            {
-                x = ex;
-            }
-
-            return x;
-        }
-
-        /// <summary>
-        /// Gets the exception thrown.
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="action">The action.</param>
-        /// <returns></returns>
-        private static Exception GetThrownException(this Action action)
-        {
-            Exception x = null;
-            try
-            {
-                action();
-            }
-            catch (Exception ex)
-            {
-                x = ex;
-            }
-
-            return x;
-        }
+      return new Verifiable<Func<T>>(function, target =>
+              Assert.IsNotNull(
+                  target.GetThrownException(),
+                  string.Format(
+                    "La función debió lanzar una excepción, pero no fue así.\n{0}", 
+                    message)));
     }
+
+    /// <summary>
+    /// Tests that an Action should throw an exception
+    /// </summary>
+    /// <param name="action">The action.</param>
+    /// <param name="message">The message.</param>
+    public static IVerifiable<Action> ThrowsException(
+      this IAssertion<Action> action, 
+      string message = "")
+    {
+      return new Verifiable<Action>(action, target =>
+      {
+        Assert.IsNotNull(target.GetThrownException(), message);
+      });
+    }
+
+    /// <summary>
+    /// Tests tha a Func<typeparamref name="T"/> does not throw an exception.
+    /// </summary>
+    /// <typeparam name="T">Return type</typeparam>
+    /// <param name="function">The function.</param>
+    /// <param name="message">The message.</param>
+    public static IVerifiable<Func<T>> DoesNotThrowException<T>(
+      this IAssertion<Func<T>> function, 
+      string message = "")
+    {
+      return new Verifiable<Func<T>>(function, target =>
+              Assert.IsNull(
+                  target.GetThrownException(),
+                  string.Format(
+                    "La función no debió lanzar una excepción, pero sí lo hizo.\n{0}", 
+                    message)));
+    }
+
+    /// <summary>
+    /// Tests that an Action should not throw an exception
+    /// </summary>
+    /// <param name="action">The action.</param>
+    /// <param name="message">The message.</param>
+    public static IVerifiable<Action> DoesNotThrowException(
+      this IAssertion<Action> action, 
+      string message = "")
+    {
+      return new Verifiable<Action>(action, target =>
+      {
+        Assert.IsNull(target.GetThrownException(), message);
+      });
+    }
+
+
+    /// <summary>
+    /// Verifies that a methods writes exactly some output to console.
+    /// </summary>
+    /// <param name="assertion">The action assertion.</param>
+    /// <param name="expectedWritten">The expected written message to console.</param>
+    /// <param name="message">The message for the error.</param>
+    /// <returns>The verification continuation</returns>    
+    public static IVerifiable<Action> WritesExactlyToConsole(
+      this IAssertion<Action> assertion,
+      string expectedWritten,
+      string message = "")
+    {
+      return new Verifiable<Action>(assertion, target =>
+      {
+
+        StringWriter consoleOut = new StringWriter();
+        Console.SetOut(consoleOut);
+        target();
+        string actualWritten = consoleOut.ToString();
+        Console.SetOut(Console.Out);
+        Assert.AreEqual(
+          actualWritten, 
+          expectedWritten, 
+          message);
+      });
+    }
+
+    /// <summary>
+    /// Verifies that a methods does not write exactly some output to console.
+    /// </summary>
+    /// <param name="assertion">The action assertion.</param>
+    /// <param name="expectedWritten">The expected message not written to console.</param>
+    /// <param name="message">The message for the error.</param>
+    /// <returns>The verification continuation</returns>    
+    public static IVerifiable<Action> DoesntWriteExactlyToConsole(
+      this IAssertion<Action> assertion,
+      string expectedWritten,
+      string message = "")
+    {
+      return new Verifiable<Action>(assertion, target =>
+      {
+
+        StringWriter consoleOut = new StringWriter();
+        Console.SetOut(consoleOut);
+        target();
+        string actualWritten = consoleOut.ToString();
+        Console.SetOut(Console.Out);
+        Assert.AreNotEqual(
+          actualWritten,
+          expectedWritten,
+          message);
+      });
+    }
+
+
+    /// <summary>
+    /// Verifies that a methods writes exactly some output to console.
+    /// </summary>
+    /// <param name="assertion">The action assertion.</param>
+    /// <param name="expectedWritten">The expected written message to console.</param>
+    /// <param name="message">The message for the error.</param>
+    /// <returns>The verification continuation</returns>    
+    public static IVerifiable<Action> WritesToConsole(
+      this IAssertion<Action> assertion,
+      string expectedWritten,
+      string message = "")
+    {
+      return new Verifiable<Action>(assertion, target =>
+      {
+
+        StringWriter consoleOut = new StringWriter();
+        Console.SetOut(consoleOut);
+        target();
+        string actualWritten = consoleOut.ToString();
+        Console.SetOut(Console.Out);
+        Assert.That(
+          actualWritten, 
+          Is.StringContaining(expectedWritten), 
+          message);
+      });
+    }
+
+    /// <summary>
+    /// Verifies that a methods does not write some output to console.
+    /// </summary>
+    /// <param name="assertion">The action assertion.</param>
+    /// <param name="expectedWritten">The written message not expected.</param>
+    /// <param name="message">The message for the error.</param>
+    /// <returns>The verification continuation</returns>    
+    public static IVerifiable<Action> DoesntWriteToConsole(
+      this IAssertion<Action> assertion,
+      string expectedWritten,
+      string message = "")
+    {
+      return new Verifiable<Action>(assertion, target =>
+      {
+
+        StringWriter consoleOut = new StringWriter();
+        Console.SetOut(consoleOut);
+        target();
+        string actualWritten = consoleOut.ToString();
+        Console.SetOut(Console.Out);
+        Assert.That(
+          actualWritten,
+          Is.Not.StringContaining(expectedWritten),
+          message);
+      });
+    }
+
+    /// <summary>
+    /// Gets the exception thrown.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="function">The function.</param>
+    /// <returns></returns>
+    private static Exception GetThrownException<T>(this Func<T> function)
+    {
+      Exception x = null;
+      try
+      {
+        function();
+      }
+      catch (Exception ex)
+      {
+        x = ex;
+      }
+
+      return x;
+    }
+
+    /// <summary>
+    /// Gets the exception thrown.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="action">The action.</param>
+    /// <returns></returns>
+    private static Exception GetThrownException(this Action action)
+    {
+      Exception x = null;
+      try
+      {
+        action();
+      }
+      catch (Exception ex)
+      {
+        x = ex;
+      }
+
+      return x;
+    }
+  }
 }
